e03d22de4de50040719c58031c699713a4d89357
pcDuino <sales@linksprite.com>
2013-03-15 10:50:14 -0600

    Initial release

---
+++ Samples/blink_led/blink_led.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# blink_led.py
+# gpio test code for pcduino ( http://www.pcduino.com )
+#
+import gpio
+import time
+
+led_pin = "gpio18"
+
+def delay(ms):
+    time.sleep(1.0*ms/1000)
+
+def setup():
+    gpio.pinMode(led_pin, gpio.OUTPUT)
+
+def loop():
+    while(1):
+        gpio.digitalWrite(led_pin, gpio.HIGH)
+        delay(100)
+        gpio.digitalWrite(led_pin, gpio.LOW)
+        delay(100)
+
+def main():
+    setup()
+    loop()
+
+main()
