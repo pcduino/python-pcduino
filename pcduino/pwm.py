89bb274071c49dd1f3608333a5664e1667c63c94
Matthew Hooker <mwhooker@gmail.com>
2013-03-24 13:42:56 -0700

    analog_write WIP

--- pcduino/pwm.py
+++ pcduino/pwm.py
@@ -1,2 +1,16 @@
-def analog_write(channel, value):
-    pass
+PWM_IF_PREFIX = "/sys/class/leds/pwm"
+PWM_IF_MAX = "max_brightness"
+PWM_IF = "brightness"
+
+MAX_PWM_LEVEL = 255
+
+#sprintf(path, "%s%d/%s", PWM_IF_PREFIX, pin, PWM_IF_MAX);
+
+def _max_value(pin):
+    return 256
+
+def analog_write(pin, value):
+    map_level = (_max_value(pin) * value) / MAX_PWM_LEVEL
+    with open("%s%d/%s" % (PWM_IF_PREFIX, pin, PWM_IF), 'w+') as f:
+        f.seek(0)
+        f.write("%d\n" % map_level)
