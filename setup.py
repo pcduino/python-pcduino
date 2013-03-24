3e15b318838e05762162573b02172eec6fcad9b3
Matthew Hooker <mwhooker@gmail.com>
2013-03-23 21:36:01 -0700

    setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pcduino",
+    version="0.1.0",
+    author="Matthew Hooker",
+    author_email="mwhooker@gmail.com",
+    url="https://github.com/pcduino/python-pcduino",
+)
