e18ec80f00e2494c27e2e904abdd85555e185d90
Johannes Ackermann <johannes.ackermann@yahoo.de>
2017-01-10 13:30:42 +0100

    Fixed pwm on V3B

--- pcduino/pwm.py
+++ pcduino/pwm.py
@@ -1,11 +1,5 @@
 import os.path
-
-#from pinmap import PinMap
-
-
-#pins = PinMap('/sys/devices/virtual/misc/pwmtimer/', 'pwm', 12)
-
-#pinMap broken beyond repair for new sysfs-pwm
+from os import listdir
 
 MAX_PWM_LEVEL = 255
 
@@ -15,26 +9,30 @@ def analog_write(pin, value):
     value can be an number between 0 and 255.
 
     """
+
     path = '/sys/class/misc/pwmtimer/'
 
     ending = 'pwm' + str(pin)
 
-    with open(os.path.join(path, 'max_level',ending )) as f:
+    pins = listdir(os.path.join(path, 'enable'))
+    if not ending in pins:
+        raise ValueError("Pin not found, PWM only possible on " + " ".join(str(p) for p in pins) + ".")
+
+    with open(os.path.join(path, 'max_level',ending)) as f:
         max_value = int(f.read())
 
     if value < 0 or value > MAX_PWM_LEVEL:
         raise ValueError("value must be between 0 and %s" % MAX_PWM_LEVEL)
 
     map_level = ((max_value-1) * value) // MAX_PWM_LEVEL
-	#// because python 3 compatibility
-   	#-1 because if it puts max_value the duty cycle somehow becomes 0 
- 
+   	#-1 because if it puts max_value the duty cycle somehow becomes 0 (overflow)
+
     #disable -> change level -> enable , as requested by documentation
     with open(os.path.join(path, 'enable', ending), 'w+') as f:
-        f.write("0\n")   
-    
+        f.write("0\n")
+
     with open(os.path.join(path, 'level', ending), 'w+') as f:
         f.write("%d\n" % map_level)
-    
+
     with open(os.path.join(path, 'enable', ending), 'w+') as f:
-        f.write("1\n")    
+        f.write("1\n")
--- pcduino/testSkriptPwm.py
+++
@@ -1,4 +0,0 @@
-from pwm import analog_write
-
-analog_write(1,100)
-
